8175e6ee61d0ee06c3530d41d7433e894f68b705
Thomas Chen <thomas99k@gmail.com>
2017-02-17 23:09:33 -0500

    Implement float regex function
    Implement function finding floats, both positive and negative from string.

---
+++ regextest.py
@@ -0,0 +1,24 @@
+import re
+
+def getWords(sentence, letter):
+	#str_regex = r"\b" + re.escape(letter) + r"\w+|\b\w+" + re.escape(letter) + r"\b"  
+
+	str_regex = "\b" + re.escape(letter) + r"\w+\b\w+" + re.escape(letter) + r"\b"
+
+	return re.findall(str_regex, sentence, re.I)
+
+def extractFloats(s):
+	return re.findall(r"[-+]?\d*\.*\d+", s, re.I)
+
+def main():
+	s = "The tires can tolerate temperatures between -32.5 and 110. That why they cost 149.95 dollars each."
+	print(s)
+	res = extractFloats(s)
+	print("Result: ", res)
+
+	t = "The TART program runs on Tuesdays and Thursdays, but it does not start until next week."
+	res = getWords(t, "t")
+	print ("Result: ", res)
+
+if __name__ == "__main__":
+	main()
