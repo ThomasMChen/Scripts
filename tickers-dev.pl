743a5fe89eccb9f42b07cb5f631b73a9845a9881
Thomas <thomas99k@gmail.com>
2017-03-12 14:48:04 -0400

    Implement test version of NYSE stock script
    Implement development version of script for direct testing with bash without Irssi dependency.

---
+++ tickers-dev.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+# resets window activity status
+#  by chent 
+#    - http://www.thomaschen.org
+
+#<scriptinfo>
+use strict;
+#use Irssi;
+use Finance::Quote;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "0.1";
+
+%IRSSI = (
+    authors	=> "chent",
+    contact	=> "chent\@thomaschen.org",
+    name	=> "Ticker",
+    description	=> "Prints Current Price of NYSE Stock via Ticker Symbol",
+    license	=> "Public Domain",
+    url		=> "",
+    changed	=> "Thu Mar 9 18:55:05 BST 2017",
+);
+
+our $q = Finance::Quote->new();
+$q->timeout(60);
+$q->require_labels(qw/price date high low volume/);
+$q->failover(1);     # Set failover support (on by default).
+
+
+sub stock{ 
+	my ($tick) = @_;
+	my %data = $q->fetch('nyse', $tick);
+	print $tick . " Current Price: " . $data{$tick, 'price'} . "\n";
+}
