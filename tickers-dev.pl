5d46c72a0f4d98a110a79047a7685e47b3e9b192
Thomas <thomas99k@gmail.com>
2017-03-19 19:44:41 -1100

    Implement price storage
    Implement storage of previous price in a hash

--- tickers-dev.pl
+++ tickers-dev.pl
@@ -26,9 +26,20 @@ $q->timeout(60);
 $q->require_labels(qw/price date high low volume/);
 $q->failover(1);     # Set failover support (on by default).
 
+our %stocks = ();
+
 
 sub stock{ 
 	my ($tick) = @_;
 	my %data = $q->fetch('nyse', $tick);
+    my $price = $data{$tick, 'price'};
+    $stocks{$tick} = $price;
 	print $tick . " Current Price: " . $data{$tick, 'price'} . "\n";
 }
+
+
+
+
+
+
+
